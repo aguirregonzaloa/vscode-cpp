a0bf01f3bcf21f73202312b5a1cfde948a504b21
Gonzalo Aguirre <gonza1810@gmail.com>
2026-06-04 12:52:45 -0300

    First commit TODO:Readme.md

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!!" << std::endl;
+    int a = 11;
+    if (a > 10)
+    {
+        std::cout << "a is greater than 10" << std::endl;
+    }
+    else
+        std::cout << "a is lower than 10" << std::endl;
+
+    return 0;
+}
